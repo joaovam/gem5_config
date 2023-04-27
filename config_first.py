176e473d21ee61a540ed5079fe788e2d5339260e
joaovam <joao.vieira.1275044@sga.pucminas.br>
2023-04-27 16:17:49 -0300

    fixing port connection

--- config_first.py
+++ config_first.py
@@ -5,75 +5,75 @@ from Cache.l1_cache import *
 from Cache.l2_cache import L2Cache
 
 
-      parser = argparse.ArgumentParser(description='A simple system with 2-level cache.')
-      parser.add_argument("binary", default="", nargs="?", type=str,
-                          help="Path to the binary to execute.")
-      parser.add_argument("--l1i_size",
-                          help=f"L1 instruction cache size. Default: 16kB.")
-      parser.add_argument("--l1d_size",
-                          help="L1 data cache size. Default: Default: 64kB.")
-      parser.add_argument("--l2_size",
-                          help="L2 cache size. Default: 256kB.")
+parser = argparse.ArgumentParser(description='A simple system with 2-level cache.')
+parser.add_argument("binary", default="", nargs="?", type=str,
+                    help="Path to the binary to execute.")
+parser.add_argument("--l1i_size",
+                    help=f"L1 instruction cache size. Default: 16kB.")
+parser.add_argument("--l1d_size",
+                    help="L1 data cache size. Default: Default: 64kB.")
+parser.add_argument("--l2_size",
+                    help="L2 cache size. Default: 256kB.")
 
-      options = parser.parse_args()
-      system = System()
+options = parser.parse_args()
+system = System()
 
-      system.clk_domain = SrcClockDomain()
-      system.clk_domain.clock = '1GHz'
-      system.clk_domain.voltage_domain = VoltageDomain()
+system.clk_domain = SrcClockDomain()
+system.clk_domain.clock = '1GHz'
+system.clk_domain.voltage_domain = VoltageDomain()
 
-      system.mem_mode = 'timing'
-      system.mem_ranges = [AddrRange('512MB')]
+system.mem_mode = 'timing'
+system.mem_ranges = [AddrRange('512MB')]
 
-      #creating CPU
-      system.cpu = X86TimingSimpleCPU()
-      system.membus = SystemXBar()
+#creating CPU
+system.cpu = X86TimingSimpleCPU()
+system.membus = SystemXBar()
 
-      #Connecting Cache
-      system.cpu.icache = L1ICache(options)
-      system.cpu.dcache = L1DCache(options)
+#Connecting Cache
+system.cpu.icache = L1ICache(options)
+system.cpu.dcache = L1DCache(options)
 
-      system.cpu.icache.connectCPU(system.cpu)
-      system.cpu.dcache.connectCPU(system.cpu)
+system.cpu.icache.connectCPU(system.cpu)
+system.cpu.dcache.connectCPU(system.cpu)
 
-      system.l2bus=L2XBar()
+system.l2bus=L2XBar()
 
-      system.cpu.icache.connectBus(system.l2bus)
-      system.cpu.dcache.connectBus(system.l2bus)
+system.cpu.icache.connectBus(system.l2bus)
+system.cpu.dcache.connectBus(system.l2bus)
 
-      system.l2cache = L2Cache(options)
-      system.l2cache.connectCPUSideBus(system.l2bus)
-      system.membus = SystemXBar()
-      system.l2cache.connectMemSideBus(system.membus)
+system.l2cache = L2Cache(options)
+system.l2cache.connectCPUSideBus(system.l2bus)
+system.membus = SystemXBar()
+system.l2cache.connectMemSideBus(system.membus)
 
-      system.cpu.createInterruptController()
-      system.cpu.interrupts[0].pio = system.membus.mem_side_ports
-      system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
-      system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
+system.cpu.createInterruptController()
+system.cpu.interrupts[0].pio = system.membus.mem_side_ports
+system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
+system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
 
-      system.system_port = system.membus.cpu_side_ports
+system.system_port = system.membus.cpu_side_ports
 
-      system.mem_ctrl = MemCtrl()
-      system.mem_ctrl.dram = DDR3_1600_8x8()
-      system.mem_ctrl.dram.range = system.mem_ranges[0]
-      system.mem_ctrl.port = system.membus.mem_side_ports
+system.mem_ctrl = MemCtrl()
+system.mem_ctrl.dram = DDR3_1600_8x8()
+system.mem_ctrl.dram.range = system.mem_ranges[0]
+system.mem_ctrl.port = system.membus.mem_side_ports
 
-      binary = 'gem5/tests/test-progs/hello/bin/x86/linux/hello'
+binary = 'gem5/tests/test-progs/hello/bin/x86/linux/hello'
 
-      # for gem5 V21 and beyond
-      system.workload = SEWorkload.init_compatible(binary)
+# for gem5 V21 and beyond
+system.workload = SEWorkload.init_compatible(binary)
 
-      process = Process()
-      process.cmd = [binary]
-      system.cpu.workload = process
-      system.cpu.createThreads()
+process = Process()
+process.cmd = [binary]
+system.cpu.workload = process
+system.cpu.createThreads()
 
-      root = Root(full_system = False, system = system)
-      m5.instantiate()
+root = Root(full_system = False, system = system)
+m5.instantiate()
 
-      print("Beginning simulation!")
-      exit_event = m5.simulate()
+print("Beginning simulation!")
+exit_event = m5.simulate()
 
-      print('Exiting @ tick {} because {}'
-            .format(m5.curTick(), exit_event.getCause()))
+print('Exiting @ tick {} because {}'
+      .format(m5.curTick(), exit_event.getCause()))
 
