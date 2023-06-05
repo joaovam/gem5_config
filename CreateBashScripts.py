147dc3ee7bdd812f68eb36dbde406c9d385c8501
joaovam <joao.vieira.1275044@sga.pucminas.br>
2023-06-05 17:41:17 -0300

    create script

---
+++ CreateBashScripts.py
@@ -0,0 +1,14 @@
+cap_programs = ["fn.intel",  "gf.intel" , "is.intel",  "km.intel",
+                "lu.intel" , "nb.intel" , "rt.intel",  "tsp.intel"]
+TOTAL_CORES = 28
+
+cap_classes = ["tiny", "small", "standard", "large", "huge"]
+
+for p in cap_programs:
+    for core in range(1,TOTAL_CORES, 1):
+        for c in cap_classes:
+            command = f"/CAPBENCH/{p} --nthread {core+1} --class {c}"
+            print(command)
+            with open(f"{p}_{c}_{core}", "w") as file:
+                file.write(command + '\n')
+                file.flush()
