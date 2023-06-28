12211457d8a039be9d3d96ed433c2271ac8d2cfa
joaovam <joao.vieira.1275044@sga.pucminas.br>
2023-06-28 07:41:36 -0300

    creating new FS script

--- CreateBashScripts.py
+++ CreateBashScripts.py
@@ -1,13 +1,13 @@
 cap_programs = ["fn.intel",  "gf.intel" , "is.intel",  "km.intel",
                 "lu.intel" , "nb.intel" , "rt.intel",  "tsp.intel"]
-TOTAL_CORES = 28
+TOTAL_CORES = 24
 
 cap_classes = ["tiny", "small", "standard", "large", "huge"]
 
 for p in cap_programs:
-    for core in range(1, TOTAL_CORES, 1):
+    for core in range(1, TOTAL_CORES + 1, 1):
         for c in cap_classes:
-            command = f"/CAPBENCH/{p} --nthread {(core+1) * 2} --class {c}"
+            command = f"/CAPBENCH/{p} --nthread {core} --class {c}"
             print(command)
             with open(f"{p}_{c}_{core}", "w") as file:
                 file.write(command + '\n')
